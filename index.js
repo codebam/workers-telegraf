653e8887795c98fd79136c62a90f70b2a7b2ed4c
Sean Behan <codebam@riseup.net>
2023-10-17 12:17:16 -0400

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { Ai } from '@cloudflare/ai';
 
 const ai = new Ai(AI);
 const bot = new Telegraf(BOT_TOKEN);
+bot.command('start', (ctx) => ctx.reply("Hello, I'm an AI, ask me a question"));
 bot.on('text', async (ctx) => {
 	const { response } = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
 		prompt: '[INST] ' + ctx.message.text + ' [/INST]',
